e7f56b139940e71953372b1aac519e4ae732a8e2
Devirob <robiszdev@gmail.com>
2022-07-28 23:42:39 +0200

    Guessing game

---
+++ guessingGame.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Shoot")
+
+	for shoots := 10; shoots > 1; shoots-- {
+
+		inputFromKeyboard, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		inputFromKeyboard = strings.TrimSpace(inputFromKeyboard)
+		shoot, err := strconv.Atoi(inputFromKeyboard)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if shoot < target {
+			fmt.Println("zamalo")
+		} else if shoot > target {
+			fmt.Println("Za duzo")
+		} else if shoot == target {
+			fmt.Println("Huirra")
+		}
+		fmt.Println("Left " + fmt.Sprint(shoots))
+
+	}
+
+}
